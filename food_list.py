e2a81122838cdecd0cfd3f3f16bd5397eb9c3795
Benjamín Straub <benjastraub@MacBook-Pro-de-Benjamin-2.local>
2019-10-24 22:46:22 -0300

    hansdlers

--- food_list.py
+++ food_list.py
@@ -128,4 +128,72 @@ class FoodList:
         self.dispatcher = self.updater.dispatcher
 
     def handlers_init(self):
+        # text messages
+        text_handler = MessageHandler(Filters.text, self.text_message)
+        self.dispatcher.add_handler(text_handler)
+        # files attachments
+        file_handler = MessageHandler(Filters.document, self.file_message)
+        self.dispatcher.add_handler(file_handler)
+        # /start
+        start_handler = CommandHandler("start", self.start)
+        self.dispatcher.add_handler(start_handler)
+        # /load_meals
+        load_meals_handler = CommandHandler("load_meals", self.load_meals)
+        self.dispatcher.add_handler(load_meals_handler)
+        # /load_ingridients
+        load_ingridients_handler = CommandHandler("load_ingridients",
+                                                  self.load_ingridients)
+        self.dispatcher.add_handler(load_ingridients_handler)
+        # /add_ingridients
+        add_ingridients_handler = CommandHandler("add_ingridients",
+                                                 self.add_ingridients)
+        self.dispatcher.add_handler(add_ingridients_handler)
+        # /remove_ingridients
+        remove_ingridients_handler = CommandHandler("remove_ingridients",
+                                                    self.remove_ingridients)
+        self.dispatcher.remove_handler(remove_ingridients_handler)
+        # /add_meals
+        add_meals_handler = CommandHandler("add_meals", self.add_meals)
+        self.dispatcher.add_handler(add_meals_handler)
+        # /stop
+        stop_handler = CommandHandler("stop", self.stop)
+        self.dispatcher.add_handler(stop_handler)
+        # /help
+        help_handler = CommandHandler("help", self.help)
+        self.dispatcher.add_handler(help_handler)
+        # unknown
+        unknown_handler = MessageHandler(Filters.command, self.unknown)
+        self.dispatcher.add_handler(unknown_handler)
+
+    def text_message(self):
+        pass
+
+    def file_message(self):
+        pass
+
+    def start_handler(self):
+        pass
+
+    def load_meals(self):
+        pass
+
+    def load_ingridients(self):
+        pass
+
+    def add_ingridients(self):
+        pass
+
+    def remove_ingridients(self):
+        pass
+
+    def add_meals(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def help(self):
+        pass
+
+    def unknown(self):
         pass
