3e6bc233582508a11518e16903566319c752ed89
Benjamín Straub <benjastraub@MacBook-Pro-de-Benjamin-2.local>
2019-10-24 21:58:27 -0300

    Spanish messages that the bot will use

---
+++ bot_messages.py
@@ -0,0 +1,25 @@
+MESSAGES = {
+    "start1": "Soy FoodList, tu asistente de compras. Cuando hayas cargado tus\
+ comidas e ingredientes, sólo selecciona las comidas que quieras y agregaré\
+ los ingredientes a tu lista de compras.",
+    "start2": "Estos son los comandos que puedes usar:\n\
+/start comienza la interacción entre nosotros\n\
+/add_meal desplegará  tus comidas para que puedas agregarlas a la lista\n\
+/add_item desplegará ingredientes para que puedas agregarlos a la lista. Si\
+ quieres agregar algo que no esté en las opciones utiliza el teclado para\
+ escribir lo que quieras\n\
+/remove_item te permite eliminar algún elemento de la lista. Sólo selecciona\
+ el que quieras sacar entre las opciones que aparecerán.\n\
+/help te explicará qué hacen los comandos\n\
+Si quieres actualizar tus ingredientes o comidas puedes volver a utilizar\
+ /load_meals y /load_ingridients. Recuerda que después de utilizar cualquiera\
+ de estos dos debes volver a utilizar /start para actualizar",
+    "start3": "Antes de empezar necesito los archivos con tus comida\
+ ingredientes. Primero escribe el comando /load_meals, presiona enviar y\
+ envía el archivo con las comidas. Después utiliza el comando\
+ /load_ingridients y envía el archivo con los ingredientes. Para terminar\
+ la configuración ocupa el comando /start",
+    "stop": "Adios",
+    "unknown": "No conozco ese comando"
+}
+MESSAGES["help"] = MESSAGES["start2"]
