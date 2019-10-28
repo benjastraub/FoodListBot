ee289ab5eaa6c1588caef8e1f34fa4bc86a8ca4c
Benjamín Straub <benjastraub@MacBook-Pro-de-Benjamin-2.local>
2019-10-28 11:28:04 -0300

    comments :pencil:

--- food_list.py
+++ food_list.py
@@ -114,6 +114,9 @@ class FoodList:
 
     @keyboard.setter
     def keyboard(self, value):
+        """
+        it assigns a telegram replay keyboard to self._keyboard
+        """
         if value is None:
             self._keyboard = telegram.ReplyKeyboardRemove()
         elif value == "add_ingridients":
@@ -179,55 +182,95 @@ class FoodList:
         self.dispatcher.add_handler(unknown_handler)
 
     def text_message(self, update, context):
+        """
+        this function handles text messages without commands. Depending on the
+        last command used it might add ingridients from a meal to the list,
+        a particular ingridient, remove an ingridient from the list or do
+        nothing
+        """
+        # check mode
         if self.adding_meals:
+            # text from the message is retrieved
             typed_meal = update.message.text
+            # we get the instance from  the meal list. It might be None
             meal = self.meal_list.get(typed_meal)
             try:
+                # might produce an AttributeError if ingridients is None
+                # every ingridient in the meal is checked
                 for ingridient in meal.ingridients:
+                    # if it's already in self.list the quantity increases
                     if ingridient.name in self.list.keys():
                         self.list[ingridient.name][1] += 1
                     else:
+                        # the instance is added to the list
                         self.list[ingridient.name] = [ingridient, 1]
+                # the list is transformed to text
                 to_write = functions.list_to_text(sorted(self.list.values(),
                                                   key=lambda x: x[0].category))
             except AttributeError:
                 to_write = MESSAGES["meal_error"]
+            # message is send
             self.send_message(update, context, to_write)
+        # check mode
         elif self.adding_ingridients:
+            # text from the message is retrieved
             typed_ingridient = update.message.text
+            # we get the instance from  the ingridients list. It might be None
             ingridient = self.ingridients.get(typed_ingridient)
             try:
+                # might produce an AttributeError if ingridients is None
+                # if it's already in self.list the quantity increases
                 if ingridient.name in self.list.keys():
                     self.list[ingridient.name][1] += 1
                 else:
+                    # the instance is added to the list
                     self.list[ingridient.name] = [ingridient, 1]
+                # the list is transformed to text
                 to_write = functions.list_to_text(sorted(self.list.values(),
                                                   key=lambda x: x[0].category))
             except AttributeError:
                 to_write = MESSAGES["add_ingridient_error"]
+            # message is send
             self.send_message(update, context, to_write)
+        # check mode
         elif self.removing_ingridients:
+            # text from the message is retrieved
             typed_ingridient = update.message.text
             try:
+                # might produce a KeyError if typed_meal is not in self.list
+                # decreases amounot of the ingridient
                 self.list[typed_ingridient][1] -= 1
+                # remove igridient from list when the quantity is 0
                 if self.list[typed_ingridient][1] == 0:
                     del self.list[typed_ingridient]
+                # the list is transformed to text
                 to_write = functions.list_to_text(sorted(self.list.values(),
                                                   key=lambda x: x[0].category))
             except KeyError:
                 to_write = MESSAGES["remove_ingridient_error"]
+            # message is send
             self.keyboard = "remove_ingridients"
             self.send_message(update, context, to_write)
 
     def file_message(self, update, context):
+        """
+        if the commands /load_meals or /load_ingridients were used, this will
+        save files with the code of the chat
+        """
+        # asigns file_tipe according to the program status
         if self.loading_meals:
             file_type = "_meals"
         elif self.load_ingridients:
             file_type = "_ingridients"
         else:
+            # if the script isn't loading_meals or loading_ingridients
+            # do a return so the function stop
             return 0
+        # unique file name is created
         file_name = str(update.message.chat.id) + file_type + ".csv"
+        # the file is retrieved from the chat
         file = context.bot.get_file(update.message.document.file_id)
+        # the file is downloaded
         file.download(file_name)
 
     def start(self, update, context):
@@ -260,63 +303,102 @@ class FoodList:
             self.send_message(update, context, to_write, False)
 
     def load_meals(self, update, context):
+        # keyboard assignment
         self.keyboard = None
+        # message is assigned
         to_write = MESSAGES["load_meals"]
+        # message is send
         self.send_message(update, context, to_write, False)
+        # set status to True and the others to False
         self.loading_meals = True
 
     def load_ingridients(self, update, context):
+        # keyboard assignment
         self.keyboard = None
+        # message is assigned
         to_write = MESSAGES["load_ingridients"]
+        # message is send
         self.send_message(update, context, to_write, False)
+        # set status to True and the others to False
         self.loading_ingridients = True
 
     def add_ingridients(self, update, context):
+        # keyboard assignment
         self.keyboard = "add_ingridients"
+        # set status to True and the others to False
         self.adding_ingridients = True
+        # message is created according to the list
         to_write = functions.list_to_text(sorted(self.list.values(),
                                           key=lambda x: x[0].category))
+        # message is send
         self.send_message(update, context, to_write)
 
     def remove_ingridients(self, update, context):
+        # keyboard assignment
         self.keyboard = "remove_ingridients"
+        # set status to True and the others to False
         self.removing_ingridients = True
+        # message is created according to the list
         to_write = functions.list_to_text(sorted(self.list.values(),
                                           key=lambda x: x[0].category))
+        # message is send
         self.send_message(update, context, to_write)
 
     def add_meals(self, update, context):
+        # keyboard assignment
         self.keyboard = "add_meals"
+        # set status to True and the others to False
         self.adding_meals = True
+        # message is created according to the list
         to_write = functions.list_to_text(sorted(self.list.values(),
                                           key=lambda x: x[0].category))
+        # message is send
         self.send_message(update, context, to_write)
 
     def see_list(self, update, context):
+        # keyboard assignment
         self.keyboard = None
+        # this makes adding_meals True
+        # and adding_ingridients and removing_ingridients False
         self.adding_meals = True
+        # then, this makes adding_ingridients False
         self.adding_meals = False
+        # message is created according to the list
         to_write = functions.list_to_text(sorted(self.list.values(),
                                           key=lambda x: x[0].category))
+        # message is send
         self.send_message(update, context, to_write)
 
     def stop(self, update, context):
+        # keyboard assignment
         self.keyboard = None
+        # message is assigned
         to_write = MESSAGES["stop"]
+        # message is send
         self.send_message(update, context, to_write, False)
         self.updater.stop()
 
     def help(self, update, context):
+        # keyboard assignment
         self.keyboard = None
+        # message is assigned
         to_write = MESSAGES["unknown"]
+        # message is send
         self.send_message(update, context, to_write, False)
 
     def unknown(self, update, context):
+        # keyboard assignment
         self.keyboard = None
+        # message is assigned
         to_write = MESSAGES["unknown"]
+        # message is send
         self.send_message(update, context, to_write, False)
 
     def send_message(self, update, context, text, markdown=True):
+        """
+        send message with text as the content of it. The function uses the
+        update and context of the chart. 
+        """
         if markdown:
             context.bot.send_message(chat_id=update.effective_chat.id,
                                      text=text, reply_markup=self.keyboard,
@@ -326,7 +408,9 @@ class FoodList:
                                      text=text, reply_markup=self.keyboard)
 
     def instance_ingridients(self, path):
+        """load the ingridients from the csv"""
         self.ingridients = functions.load_ingridients(path)
 
     def instance_meals(self, path):
+        """Load the meals from the csv"""
         self.meal_list = functions.load_meals(path, self.ingridients)
